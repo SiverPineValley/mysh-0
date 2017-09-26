9e131c2ae99549e4551999b64a599bbb36e3b8f6
Park Jong in <whddls9632@naver.com>
2017-09-27 00:18:36 +0900

    utils.c first coding

--- src/utils.c
+++ src/utils.c
@@ -1,7 +1,20 @@
 #include "utils.h"
+#include <stdio.h>
+#include <string.h>
 
 void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
 {
-  // TODO: Fill this!
+	int temp = 0;
+	char* stemp = strtok( command  , " " ); 
+	
+	while (stemp != NULL) {	
+		strcpy(**(argv + temp),stemp);
+		stemp = strtok(NULL, " ");
+		temp++;
+	
+}
+	*argc = temp;
+
+
 }
